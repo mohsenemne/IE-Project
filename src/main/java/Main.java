23c058731391a47914843bfb396c9fa998c3d8fe
Taha Bagheri <tahabagheri@Tahas-MacBook-Pro.local>
2019-02-13 16:32:05 +0330

    User class added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,56 @@
 import javafx.util.Pair;
 
-import java.util.Scanner;
+import java.util.*;
+
+
+
+class Skill {
+    private String name ;
+    private int point ;
+
+    public Skill (String name, int point) {
+        this.name = name ;
+        this.point = point ;
+    }
+
+    public int getPoint (Skill s){
+        if (this.name.equals(s.name)) {
+            return this.point - s.point ;
+        }
+        else
+            return 0 ;
+    }
+}
+
+
+class User {
+    private String username ;
+    //private  HashMap<String, int> skills ;
+    private Vector<Skill> skills ;
+
+    public User (String username, Vector<Skill> skills) {
+        this.username = username ;
+        this.skills = skills ;
+    }
+
+    public int skillsPoint(Vector<Skill> requiredSkills) {
+        int result = 0;
+        //Iterator it = requiredSkills.iterator();
+        for (int i=0; i<requiredSkills.size(); i++){
+            for (int j=0; j<this.skills.size(); j++){
+                int point = this.skills.get(j).getPoint(requiredSkills.get(i));
+                if (point < 0){
+                    return -1;
+                }
+                else if(point > 0){
+                    result += point*point;
+                }
+            }
+        }
+        return result*10000;
+    }
+}
+
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
